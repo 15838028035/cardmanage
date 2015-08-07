2597a199bc16f1901e5e6794d4e4db48949bc448
liujie <liujie09_24@163.com>
2015-08-07 20:51:24 +0800

    add user test

--- src/test/java/com/lj/app/cardmanage/user/service/UserServiceTest.java
+++ src/test/java/com/lj/app/cardmanage/user/service/UserServiceTest.java
@@ -11,14 +11,20 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lj.app.cardmanage.user.model.User;
 import com.lj.app.core.common.pagination.Page;
 import com.lj.app.core.common.pagination.PageTool;
+import com.lj.app.core.common.util.DateUtil;
+import com.lj.app.core.common.util.des.DesUtil;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring-base.xml")
+@Transactional  
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)  
 public class UserServiceTest {
 
 	@Autowired
@@ -44,6 +50,19 @@ public class UserServiceTest {
 		Assert.assertEquals(10, userList.size());
 		String result = PageTool.pageToJsonJQGrid(pageUser);
 		System.out.println("result=" +result);
+	}
+	
+	@Test
+	public void insetTest() {
+		User user = new User();
+		user.setLoginNo("111");
+		user.setUserName("user111");
+		user.setCardNo("11001");
+		user.setAddress("111");
+		user.setMobile("1111111");
+		user.setCreateBy(1);
+		user.setCreateDate(DateUtil.getNowDateYYYYMMddHHMMSS());
+		userService.insertObject(user);
 	}
 	
 }
